ad262d1945bf5bce34893fa40a204b2d6f56cf5e
Joe Love <git@joelove.uk>
2016-01-25 22:44:54 +0000

    add sample code

--- dist/run.js
+++ dist/run.js
@@ -1,4 +1,21 @@
 #! /usr/bin/env node
 'use strict';
 
-console.log('WOOF');
+var _net = require('net');
+
+var _net2 = _interopRequireDefault(_net);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var client = _net2.default.connect('1111', 'lemondigits.com');
+
+client.on('data', function (data) {
+  console.log('' + data);
+  process.stdin.once('data', function (chunk) {
+    client.write(chunk.toString());
+  });
+}).on('connect', function () {
+  console.log('Connected!');
+}).on('end', function () {
+  console.log('Disconnected!');
+});
--- src/run.js
+++ src/run.js
@@ -1,3 +1,16 @@
 #! /usr/bin/env node
 
-console.log('WOOF');
+import net from 'net';
+
+const client = net.connect('1111', 'lemondigits.com');
+
+client.on('data', data => {
+  console.log('' + data);
+  process.stdin.once('data', chunk => {
+    client.write(chunk.toString());
+  });
+}).on('connect', () => {
+  console.log('Connected!');
+}).on('end', () => {
+  console.log('Disconnected!');
+});
