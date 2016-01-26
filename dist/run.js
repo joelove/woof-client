6aab127243be82f2fc971335ff28f44189b59ce4
Joe Love <git@joelove.uk>
2016-01-26 01:21:33 +0000

    POC WIP

--- dist/run.js
+++ dist/run.js
@@ -5,17 +5,96 @@ var _net = require('net');
 
 var _net2 = _interopRequireDefault(_net);
 
+var _blessed = require('blessed');
+
+var _blessed2 = _interopRequireDefault(_blessed);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var screen = _blessed2.default.screen({ smartCSR: true });
 var client = _net2.default.connect('1111', 'lemondigits.com');
 
-client.on('data', function (data) {
-  console.log(data.toString());
-  process.stdin.once('data', function (chunk) {
-    client.write(chunk.toString());
-  });
-}).on('connect', function () {
-  console.log('Connected!');
-}).on('end', function () {
-  console.log('Disconnected!');
+var buffer = _blessed2.default.text({
+  top: 0,
+  left: 0,
+  width: '80%',
+  height: '100%-2',
+  scrollable: true,
+  style: {
+    fg: 'black',
+    bg: '#f0f0f0'
+  }
 });
+
+var input = _blessed2.default.textbox({
+  bottom: 1,
+  left: 0,
+  width: '80%',
+  height: 1,
+  inputOnFocus: true,
+  style: {
+    fg: 'black',
+    bg: '#d3d3d3',
+    hover: {
+      bg: '#b3b3b3'
+    },
+    focus: {
+      bg: '#b3b3b3'
+    }
+  }
+});
+
+var sidebar = _blessed2.default.list({
+  right: 0,
+  top: 0,
+  width: '20%',
+  height: '100%-1',
+  style: {
+    fg: 'black',
+    bg: '#fafafa'
+  }
+});
+
+var statusbar = _blessed2.default.box({
+  bottom: 0,
+  width: '100%',
+  height: 1,
+  style: {
+    fg: 'black',
+    bg: '#999999'
+  }
+});
+
+screen.title = 'WOOF';
+screen.append(buffer);
+screen.append(input);
+screen.append(sidebar);
+screen.append(statusbar);
+
+input.on('click', function (data) {
+  input.focus();
+  screen.render();
+});
+
+screen.key(['escape', 'C-c'], function (ch, key) {
+  return process.exit(0);
+});
+
+client.on('data', function (data) {
+  buffer.pushLine(data.toString());
+  screen.render();
+}).on('connect', function () {
+  // console.log('Connected!');
+}).on('end', function () {
+  // console.log('Disconnected!');
+});
+
+input.key('enter', function (ch, key) {
+  client.write(input.getContent());
+  input.clearValue();
+  input.focus();
+  screen.render();
+});
+
+input.focus();
+screen.render();
--- src/run.js
+++ src/run.js
@@ -1,16 +1,92 @@
 #! /usr/bin/env node
 
 import net from 'net';
+import blessed from 'blessed';
 
+const screen = blessed.screen({ smartCSR: true });
 const client = net.connect('1111', 'lemondigits.com');
 
-client.on('data', data => {
-  console.log(data.toString());
-  process.stdin.once('data', chunk => {
-    client.write(chunk.toString());
-  });
-}).on('connect', () => {
-  console.log('Connected!');
-}).on('end', () => {
-  console.log('Disconnected!');
+const buffer = blessed.text({
+  top: 0,
+  left: 0,
+  width: '80%',
+  height: '100%-2',
+  scrollable: true,
+  style: {
+    fg: 'black',
+    bg: '#f0f0f0'
+  }
 });
+
+const input = blessed.textbox({
+  bottom: 1,
+  left: 0,
+  width: '80%',
+  height: 1,
+  inputOnFocus: true,
+  style: {
+    fg: 'black',
+    bg: '#d3d3d3',
+    hover: {
+      bg: '#b3b3b3'
+    },
+    focus: {
+      bg: '#b3b3b3'
+    }
+  }
+});
+
+const sidebar = blessed.list({
+  right: 0,
+  top: 0,
+  width: '20%',
+  height: '100%-1',
+  style: {
+    fg: 'black',
+    bg: '#fafafa'
+  }
+});
+
+const statusbar = blessed.box({
+  bottom: 0,
+  width: '100%',
+  height: 1,
+  style: {
+    fg: 'black',
+    bg: '#999999'
+  }
+});
+
+screen.title = 'WOOF';
+screen.append(buffer);
+screen.append(input);
+screen.append(sidebar);
+screen.append(statusbar);
+
+input.on('click', function(data) {
+  input.focus();
+  screen.render();
+});
+
+screen.key(['escape', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+client.on('data', data => {
+  buffer.pushLine(data.toString());
+  screen.render();
+}).on('connect', () => {
+  // console.log('Connected!');
+}).on('end', () => {
+  // console.log('Disconnected!');
+});
+
+input.key('enter', function(ch, key) {
+  client.write(input.getContent());
+  input.clearValue();
+  input.focus();
+  screen.render();
+});
+
+input.focus();
+screen.render();
