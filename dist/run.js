8ac651b40f6172c6e7d111250ad798a2b8512e92
Joe Love <git@joelove.uk>
2016-01-27 13:43:52 +0000

    add log

--- dist/run.js
+++ dist/run.js
@@ -9,57 +9,62 @@ var _blessed = require('blessed');
 
 var _blessed2 = _interopRequireDefault(_blessed);
 
+var _blessedContrib = require('blessed-contrib');
+
+var _blessedContrib2 = _interopRequireDefault(_blessedContrib);
+
+var _default = require('../themes/default');
+
+var _default2 = _interopRequireDefault(_default);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var screen = _blessed2.default.screen({ smartCSR: true });
-var client = _net2.default.connect('1111', 'lemondigits.com');
-var theme = require('../themes/default.json');
-
-var buffer = _blessed2.default.box(theme.buffer);
-var input = _blessed2.default.textbox(theme.input);
-var sidebar = _blessed2.default.list(theme.sidebar);
-var statusbar = _blessed2.default.box(theme.statusbar);
+var input = _blessed2.default.textbox(_default2.default.input);
+var buffer = _blessed2.default.box(_default2.default.buffer);
+var sidebar = _blessed2.default.list(_default2.default.sidebar);
+var statusbar = _blessed2.default.box(_default2.default.statusbar);
+var log = _blessedContrib2.default.log(_default2.default.log);
 
 var exit = function exit() {
   return process.exit(0);
 };
+var focus = function focus() {
+  return input.focus();
+};
 var connected = function connected() {
-  return append('Connection established!');
+  return log.log('Connection established!');
 };
 
 var append = function append(data) {
+  log.log('DATA:' + data);
   buffer.pushLine(data.toString());
-  screen.render();
 };
 
-var focus = function focus() {
-  input.focus();
-  screen.render();
-};
-
-var clear = function clear() {
+var write = function write() {
+  var line = input.getContent();
+  log.log('INPUT:' + line);
+  client.write(line);
   input.clearValue();
   focus();
 };
 
-var write = function write() {
-  client.write(input.getContent());
-  clear();
-};
+screen.title = 'WOOF';
+screen.key(['escape'], exit);
+screen.append(buffer);
+screen.append(input);
+screen.append(sidebar);
+screen.append(statusbar);
+screen.append(log);
 
 input.on('click', focus);
-input.key(['escape'], exit);
 input.key('enter', write);
+input.focus();
+
+var client = _net2.default.connect('1111', 'lemondigits.com');
 
 client.on('data', append);
 client.on('connect', connected);
 client.on('end', exit);
 
-screen.title = 'WOOF';
-screen.append(buffer);
-screen.append(input);
-screen.append(sidebar);
-screen.append(statusbar);
-
-input.focus();
 screen.render();
--- src/run.js
+++ src/run.js
@@ -2,52 +2,49 @@
 
 import net from 'net';
 import blessed from 'blessed';
+import contrib from 'blessed-contrib';
+import theme from '../themes/default';
 
 const screen = blessed.screen({ smartCSR: true });
-const client = net.connect('1111', 'lemondigits.com');
-const theme = require('../themes/default.json');
-
-const buffer = blessed.box(theme.buffer);
 const input = blessed.textbox(theme.input);
+const buffer = blessed.box(theme.buffer);
 const sidebar = blessed.list(theme.sidebar);
 const statusbar = blessed.box(theme.statusbar);
+const log = contrib.log(theme.log);
 
 const exit = () => process.exit(0);
-const connected = () => append('Connection established!');
+const focus = () => input.focus();
+const connected = () => log.log('Connection established!');
 
 const append = data => {
+  log.log('DATA:' + data);
   buffer.pushLine(data.toString());
-  screen.render();
 }
 
-const focus = () => {
-  input.focus();
-  screen.render();
-}
-
-const clear = () => {
+const write = () => {
+  const line = input.getContent();
+  log.log('INPUT:' + line);
+  client.write(line);
   input.clearValue();
   focus();
 }
 
-const write = () => {
-  client.write(input.getContent());
-  clear();
-}
+screen.title = 'WOOF';
+screen.key(['escape'], exit);
+screen.append(buffer);
+screen.append(input);
+screen.append(sidebar);
+screen.append(statusbar);
+screen.append(log);
 
 input.on('click', focus);
-input.key(['escape'], exit);
 input.key('enter', write);
+input.focus();
+
+const client = net.connect('1111', 'lemondigits.com');
 
 client.on('data', append);
 client.on('connect', connected);
 client.on('end', exit);
 
-screen.title = 'WOOF';
-screen.append(buffer);
-screen.append(input);
-screen.append(sidebar);
-screen.append(statusbar);
-
-input.focus();
 screen.render();
